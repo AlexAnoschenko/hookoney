881703e2e6b0bf22325af54ba1a1fa4e33ac59f3
Aliaksei Anoshchanka <aliaksei.anoshchanka@aventri.com>
2021-07-13 01:32:38 +0300

    added db

--- src/App.js
+++ src/App.js
@@ -1,9 +1,79 @@
+import { useState, useEffect } from "react";
+import firebase from "firebase";
 import "./App.css";
 
 function App() {
+  const [state, setState] = useState({
+    email: "",
+    password: "",
+    hasAccount: false,
+    name: "",
+  });
+
+  const handleChange = ({ target: { value, id } }) => {
+    setState({ ...state, [id]: value });
+  };
+
+  const createAccount = () => {
+    const { email, password } = state;
+    // firebase.auth().createUserWithEmailAndPassword(email, password);
+
+    firebase
+      .auth()
+      .signInWithEmailAndPassword(email, password)
+      .then((response) => {
+        setState({ ...state, hasAccount: true });
+      });
+  };
+
+  useEffect(() => {
+    // const db = firebase.database();
+
+    var starCountRef = firebase.database().ref("name");
+
+    console.log(starCountRef);
+
+    starCountRef.on("value", (snapshot) => {
+      const data = snapshot.val();
+      console.log(data);
+    });
+
+    // const name = db.ref("name");
+    // name.on("value", (elem) => {
+    //   setState({ ...state, name: elem.val() });
+    // });
+
+    // console.log(state);
+  });
+
   return (
-    <div className="App">
-      <div>Hookoney!</div>
+    <div className="App bg-black">
+      <div className="text-white mb-4">Hookoney!</div>
+      {state.hasAccount ? (
+        <div className="text-white">{`Hello, ${state.name}!`}</div>
+      ) : (
+        <div className="flex flex-col">
+          <input
+            id="email"
+            type="text"
+            className="mb-2 rounded"
+            onChange={handleChange}
+          />
+          <input
+            id="password"
+            type="password"
+            className="mb-2 rounded"
+            onChange={handleChange}
+          />
+          <button
+            type="submit"
+            className="text-white rounded border border-white"
+            onClick={createAccount}
+          >
+            Submit
+          </button>
+        </div>
+      )}
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,14 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import firebase from "firebase";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCuerGBewQkdfa3k_3uLI3ope4RaT_sizI",
+  authDomain: "hookoney-c50a4.firebaseapp.com",
+  projectId: "hookoney-c50a4",
+  storageBucket: "hookoney-c50a4.appspot.com",
+  messagingSenderId: "142885839129",
+  appId: "1:142885839129:web:a4100b51132c461bc66588",
+  measurementId: "G-X5JRQNLG1S",
+};
+
+firebase.initializeApp(firebaseConfig);
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
